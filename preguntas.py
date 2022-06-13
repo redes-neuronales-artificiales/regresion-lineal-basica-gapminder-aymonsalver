49d63c24370f3e7842f2a28fc46fea6872b74b96
Angelo Yesid <aymonsalver@unal.edu.co>
2022-06-12 20:00:50 -0500

    Pregunta3_Llenado1

--- preguntas.py
+++ preguntas.py
@@ -82,35 +82,40 @@ def pregunta_03():
     """
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = ____
+    df = pd.read_csv(
+        "gm_2008_region.csv",
+        sep=",",
+        thousands=None,
+        decimal=".",
+        )
 
     # Asigne a la variable los valores de la columna `fertility`
-    X_fertility = ____
+    X_fertility = np.array(df["fertility"]).reshape(-1, 1)
 
     # Asigne a la variable los valores de la columna `life`
-    y_life = ____
+    y_life = np.array(df["life"]).reshape(-1, 1)
 
     # Importe LinearRegression
-    from ____ import ____
+    from sklearn.linear_model import LinearRegression
 
     # Cree una instancia del modelo de regresión lineal
-    reg = ____
+    reg = LinearRegression()
 
     # Cree El espacio de predicción. Esto es, use linspace para crear
     # un vector con valores entre el máximo y el mínimo de X_fertility
-    prediction_space = ____(
-        ____,
-        ____,
-    ).reshape(____, _____)
+    prediction_space = np.linspace(
+        max(X_fertility),
+        min(X_fertility),
+    ).reshape(-1, 1)
 
     # Entrene el modelo usando X_fertility y y_life
-    reg.fit(____, ____)
+    reg.fit(X_fertility, y_life)
 
     # Compute las predicciones para el espacio de predicción
     y_pred = reg.predict(prediction_space)
 
     # Imprima el R^2 del modelo con 4 decimales
-    print(____.score(____, ____).round(____))
+    print(reg.score(X_fertility, y_life).round(4))
 
 
 def pregunta_04():
